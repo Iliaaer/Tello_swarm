1bf7baa280fd711220aab4b78e098e5ddc0e013f
Iliaaer <31032527+Iliaaer@users.noreply.github.com>
2021-08-27 01:49:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 
-# For more details and step by step guide visit: Microcontrollerslab.com
-
 try:
   import usocket as socket
 except:
   import socket
 
 from machine import Pin
+from neopixel import NeoPixel
+import time
 import network
 
 import esp
@@ -15,9 +15,15 @@ esp.osdebug(None)
 import gc
 gc.collect()
 
-ssid = 'N300 N1zi'
-password = 'loragera1'
+#ssid = 'N300 N1zi'
+#password = 'loragera1'
+
+ssid = 'show'
+password = 'ilia2002'
+
 nameArduino = "Tello1"
+
+n = 26
 
 station = network.WLAN(network.STA_IF)
 
@@ -32,6 +38,8 @@ print(station.ifconfig())
 
 led = Pin(2, Pin.OUT)
 
+pin = Pin(14, Pin.OUT)   # set GPIO14 to output to drive NeoPixels
+np = NeoPixel(pin, n)   # create NeoPixel driver on GPIO14 for 8 pixels
 
 led_state = "OFF"
 def web_page(text:str):
@@ -41,14 +49,7 @@ def web_page(text:str):
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
      integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-    <style>
-        html {
-            font-family: Arial;
-            display: inline-block;
-            margin: 0px auto;
-            text-align: center;
-        }
-    </style>
+
 </head>
 
 <body>
@@ -58,6 +59,10 @@ def web_page(text:str):
 </html>"""
     return html
 
+
+for i in range(n):
+  np[i] = (0, 0, 0)
+np.write() 
 
 
 
@@ -75,23 +80,35 @@ while True:
         conn.settimeout(None)
         request_d = request.decode("utf-8")
         request = str(request)
+        #print('GET Rquest Content = %s' % request)
         led_on = request.find('/?led_2_on')
         led_off = request.find('/?led_2_off')
         
-      
+        
+        
         if led_on == 6:
-            print('LED ON -> GPIO2')  # Выключение
-            print('Close')            
+            print('LED ON -> GPIO2')  #lose')            
             led_state = "OFF"
             led.on() 
+            
+            for i in range(n):
+              np[i] = (0, 0, 0)
+            np.write()    
         if led_off == 6:
-            print('LED OFF -> GPIO2') # Включение
+            print('LED OFF -> GPIO2')
             print('Open')
             led_state = "ON"
 
             led.off()
             r, g, b = request_d[16:request_d.find(' HTTP')].split('_')
-            print('red = {}, green = {}, blue = {}'.format(r, g, b))  
+            r, g, b = int(r), int(g), int(b)
+            print('red = {}, green = {}, blue = {}'.format(r, g, b))
+            r = r if 0 <= r <= 255 else 0 if r < 0 else 255
+            g = g if 0 <= g <= 255 else 0 if g < 0 else 255
+            b = b if 0 <= b <= 255 else 0 if b < 0 else 255
+            for i in range(n):
+              np[i] = (r, g, b)
+            np.write()    
         response = web_page(nameArduino)
         conn.send('HTTP/1.1 200 OK\n')
         conn.send('Content-Type: text/html\n')
@@ -101,3 +118,21 @@ while True:
     except OSError as e:
         conn.close()
         print('Connection closed')
+
+'''from machine import Pin
+
+
+n = 26
+pin = Pin(14, Pin.OUT)   # set GPIO14 to output to drive NeoPixels
+np = NeoPixel(pin, n)   # create NeoPixel driver on GPIO14 for 8 pixels
+# np[0] = (255, 0, 0) # set the first pixel to white
+for i in range(n):
+  np[i] = (0, 0, 255)
+#np.fill((0, 0, 255))
+
+np.write()              # write data to all pixels
+
+time.sleep_ms(1000)
+'''
+
+
